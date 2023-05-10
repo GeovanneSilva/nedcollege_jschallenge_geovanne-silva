c2f7eba2b360e4dce2d78bdb5916f733b279d67a
Geovanne Silva <geovanne.08@gmail.com>
2023-05-10 20:51:34 +0100

    trying to fix a bug with connection

--- api/index.js
+++ api/index.js
@@ -1,13 +1,32 @@
 import express from "express";
+import cookieParser from "cookie-parser"
+import multer from "multer"
 import postRoutes from "./routes/posts.js"
 import usersRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
-import cookieParser from "cookie-parser";
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, Date.now()+file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    const file = req.file;
+    res.status(200).json(file.filename);
+})
+
 app.use("/api/auth", authRoutes)
 app.use("/api/users", usersRoutes)
 app.use("/api/posts", postRoutes)
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,8 +1,12 @@
 import express from "express"
-import { addPost } from "../controllers/post.js"
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.js"
 
 const router = express.Router()
 
-router.get("/test", addPost)
+router.get("/", getPosts)
+router.get("/:id", getPost)
+router.post("/add", addPost)
+router.put("/update/:id", updatePost)
+router.delete("/:id", deletePost)
 
 export default router
